62ba29cd0353e09a5164464aeca13cbbe6589f7d
sid <siddhantkothari076@gmail.com>
2023-05-01 22:41:25 +0530

    initial commit

---
+++ src/data/priceRanges.js
@@ -0,0 +1,42 @@
+export const prices = [
+    {
+        "id": 0,
+        "name": "Any",
+        "array": [0,50000]
+    },
+    {
+        "id": 1,
+        "name": "Rs.5Rs. to Rs.150",
+        "array": [50, 150]
+    },
+    {
+        "id": 2,
+        "name": "Rs.151 to Rs.250",
+        "array": [151, 250]
+    },
+    {
+        "id": 3,
+        "name": "Rs.251 to Rs.350",
+        "array": [251, 350]
+    },
+    {
+        "id": 4,
+        "name": "Rs.351 to Rs.450",
+        "array": [351, 450]
+    },
+    {
+        "id": 5,
+        "name": "Rs.451 to Rs.650",
+        "array": [451, 650]
+    },
+    {
+        "id": 6,
+        "name": "Rs.651 to Rs.850",
+        "array": [651, 850]
+    },
+    {
+        "id": 7,
+        "name": "More than Rs.851",
+        "array": [851, 50000]
+    }
+]
